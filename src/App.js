d55b18dd43118f14200e144805b676c11b6418c4
EricHong <540940964@qq.com>
2024-10-12 13:10:45 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Link,
+  Routes,
+  Route
+} from 'react-router-dom';
+import {
+  KeepAlive,
+  keepAliveTransfer
+} from './KeepAlive';
+
+import HomeView from './views/Home';
+import FromView from './views/Form';
+
+const AliveHomeView = keepAliveTransfer(HomeView, 'home');
+const AliveFormView = keepAliveTransfer(FromView, 'form');
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <KeepAlive>
+        <div>
+          <ul id="nav">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Routes>
+            <Route path='/' element={<AliveHomeView />}></Route>
+            <Route path='/form' element={<AliveFormView />}></Route>
+          </Routes>
+        </div>
+      </KeepAlive>
+    </BrowserRouter>
+  )
 }
 
 export default App;
